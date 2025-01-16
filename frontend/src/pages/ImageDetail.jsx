03198e5bdffc1cef2aaa486c491c0430604a0fe6
Rakibul Islam <rakib.ca@gmail.com>
2025-01-15 22:55:44 -0700

    styling updates

--- frontend/src/pages/ImageDetail.jsx
+++ frontend/src/pages/ImageDetail.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "animate.css";
+import Flex from "@react-css/flex";
 import Alert from "react-bootstrap/Alert";
 import PayPalIntegration from "../components/PayPalIntegration";
 
@@ -26,45 +27,54 @@ const ImageDetail = () => {
   }, [id]);
 
   return (
-    <div className="d-flex flex-column align-items-center w-50 m-auto mt-4">
-      <div className="image-card">
-        <img
-          src={imageDetails?.imageURL}
-          alt={imageDetails?.originalName}
-          style={{ borderWidth: "16px" }}
-        />
-      </div>
+    <Flex column alignItemsCenter>
+      <Flex.Item className="image-detail">
+        <img src={imageDetails?.imageURL} alt={imageDetails?.originalName} />
+      </Flex.Item>
       <br></br>
-      <h4 style={{ color: "#ceb4a9" }}>
-        {imageDetails?.originalName
-          .split(".")
-          .slice(0, -1)
-          .join(".")
-          .split(" ")
-          .slice(1, -1)
-          .join(" ")}
-      </h4>
+
+      <Flex.Item>
+        <h4 style={{ color: "#ceb4a9" }}>
+          {imageDetails?.originalName
+            .split(".")
+            .slice(0, -1)
+            .join(".")
+            .split(" ")
+            .slice(1, -1)
+            .join(" ")}
+        </h4>
+      </Flex.Item>
       <br></br>
-      <h4 style={{ color: "#ceb4a9" }}>
-        {"Price: $" + imageDetails?.originalName.split(" ").pop().slice(0, -5)}
-      </h4>
+
+      <Flex.Item>
+        <h4 style={{ color: "#ceb4a9" }}>
+          {"Price: $" +
+            imageDetails?.originalName.split(" ").pop().slice(0, -5)}
+        </h4>
+      </Flex.Item>
       <br></br>
-      <PayPalIntegration />
+
+      <Flex.Item>
+        <PayPalIntegration />
+      </Flex.Item>
       <br></br>
-      <Alert
-        variant="warning"
-        className="animate__animated animate__fadeInDown"
-      >
-        <p>
-          Please send me a message if you would like to purchase this art work
-          by e-transfer.
-        </p>
-      </Alert>
+
+      <Flex.Item>
+        <Alert
+          variant="warning"
+          className="animate__animated animate__fadeInDown"
+        >
+          <p>
+            Please send me a message if you would like to purchase this art work
+            by e-transfer.
+          </p>
+        </Alert>
+      </Flex.Item>
 
       <br></br>
       <br></br>
       <br></br>
-    </div>
+    </Flex>
   );
 };
 

70cf81915de29072dc899dd4781666a6a0b1ac36
Rakibul Islam <rakib.ca@gmail.com>
2025-01-29 21:17:38 -0700

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const https = require("https");
-const fs = require("fs");
+//const https = require("https");
+//const fs = require("fs");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const cors = require("cors");
@@ -9,10 +9,10 @@ const ImageRoutes = require("./Routes/ImageRoutes");
 const PORT = process.env.PORT;
 require("./Models/db");
 
-const options = {
-  key: fs.readFileSync("/etc/letsencrypt/live/raonak.ca/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/raonak.ca/fullchain.pem"),
-};
+// const options = {
+//   key: fs.readFileSync("/etc/letsencrypt/live/raonak.ca/privkey.pem"),
+//   cert: fs.readFileSync("/etc/letsencrypt/live/raonak.ca/fullchain.pem"),
+// };
 
 app.get("/", (req, res) => {
   res.send("Backend server is running !!");
@@ -33,10 +33,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/images", ImageRoutes);
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on PORT: ${PORT}`);
-// });
-
-https.createServer(options, app).listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
 });
+
+// https.createServer(options, app).listen(3000, () => {
+//   console.log("Server listening on port 3000");
+// });

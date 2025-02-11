1cc206587b3e41f99b30bec19665368283f79b23
Rakibul Islam <rakib.ca@gmail.com>
2025-02-11 00:25:18 -0700

    added framer-motion

--- frontend/src/components/AllImageDisplay.jsx
+++ frontend/src/components/AllImageDisplay.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const AllImageDisplay = ({ imageGallery }) => {
   return (
     <div className="image-grid">
       {imageGallery.map((image, index) => (
-        <div key={index} className="image-card">
+        <motion.div
+          key={index}
+          className="image-card"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
           <Link to={`/${image._id}`}>
             <img src={image.imageURL} alt={image.originalName} />
           </Link>
-        </div>
+        </motion.div>
       ))}
     </div>
   );

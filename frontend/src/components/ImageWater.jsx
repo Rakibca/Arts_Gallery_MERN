b636c61bd114768dce246ffb555efe1de7f5dbcc
Rakibul Islam <rakib.ca@gmail.com>
2025-02-12 22:00:45 -0700

    minor changes

--- frontend/src/components/ImageWater.jsx
+++ frontend/src/components/ImageWater.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+// import {
+//   LazyLoadImage,
+//   trackWindowScroll,
+// } from "react-lazy-load-image-component";
 
 const ImageWater = ({ images }) => {
   return (
@@ -24,3 +28,22 @@ const ImageWater = ({ images }) => {
 };
 
 export default ImageWater;
+
+// const ImageWater = ({ images, scrollPosition }) => (
+//   <div className="image-grid image-card">
+//     {images
+//       .filter((image) => image.originalName.includes("Water"))
+//       .map((image, index) => (
+//         <Link to={`/${image._id}`}>
+//           <LazyLoadImage
+//             key={index}
+//             src={image.imageURL}
+//             alt={image.originalName}
+//             scrollPosition={scrollPosition}
+//           />
+//         </Link>
+//       ))}
+//   </div>
+// );
+
+// export default trackWindowScroll(ImageWater);

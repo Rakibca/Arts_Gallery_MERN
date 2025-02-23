f9a7dc4d66e089e1051470073cb2c38a2579a5e4
Rakibul Islam <rakib.ca@gmail.com>
2025-02-23 09:21:00 -0700

    minor changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import "react-toastify/ReactToastify.css";
 
+if (window.location.host === "www.raonak.ca") {
+  setTimeout(() => {
+    window.location.href = "https://raonak.ca" + window.location.pathname;
+  }, 2000); // 2000ms = 2s
+}
+
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-    <App />
+  <App />
   // </StrictMode>
 );
